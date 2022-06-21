bfadb9230c623cf778bb02cfb7d776a16d750087
Tiago Kazuo Lino <tiagokazuolino@MacbookProTiagoGaming3i.local>
2022-06-21 20:46:51 -0300

    first commit

---
+++ PS2022 v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PS2022 v2
+//
+//  Created by Tiago Kazuo Lino on 06/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
